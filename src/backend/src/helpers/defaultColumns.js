3945a97982a7d08ff171f41b7625d56b68921cf5
L00p1nLup1n <lykhoidu@gmail.com>
2026-02-25 15:12:43 +0700

    refactor: move default columns template as seperate helper

---
+++ src/backend/src/helpers/defaultColumns.js
@@ -0,0 +1,5 @@
+export const DEFAULT_COLUMNS_TEMPLATE = [
+  { key: 'todo', title: 'To do', order: 1 },
+  { key: 'inprogress', title: 'In Progress', order: 2 },
+  { key: 'done', title: 'Done', order: 3 },
+];
